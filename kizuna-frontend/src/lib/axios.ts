f31d13f1d5842060dfbd6ab89acf271616715928
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-02-10 01:00:30 +0530

    issues

--- kizuna-frontend/src/lib/axios.ts
+++ kizuna-frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://kizuna.onrender.com/",
+    baseURL: "https://kizuna.onrender.com/api",
     withCredentials: true
 })
