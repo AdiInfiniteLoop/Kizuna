2c78e5901857154d4d6bcd3b253aa27a304f54e7
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-02-10 01:17:00 +0530

    issues

--- kizuna-backend/src/lib/redis.ts
+++ kizuna-backend/src/lib/redis.ts
@@ -4,8 +4,8 @@ const client = new Redis({
     port: 6379,
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD,
-    username: process.env.REDIS_USER, // Optional, depending on your Redis setup
-    tls: {} // Enable secure connection (rediss://)
+    username: process.env.REDIS_USER, 
+    tls: { rejectUnauthorized: false} 
   });
 
 client.on('connect', () => console.log('✅ Connected to Redis'));
